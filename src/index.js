ae69196bfd582697e979f935e3ee4b67f4da934f
Giotu <evilalex1998@mail.ru>
2022-06-23 03:07:12 +0300

    comlete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result ='';
+  let num = Math.abs(n).toString();
+  for(let i = 0; i < num.length; i++){
+    result += num[num.length-1-i]; 
+  }
+  return +result;
 }
